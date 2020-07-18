c7caee6d18504521a9fcd36567423206097a2d0f
DevVatu <devvatu@gmail.com>
2020-07-18 16:16:31 +0200

    Updates

---
+++ precache-manifest.1d8da25644378122e07a3b74ab57c5d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8339de3048e3df0add79deabaf20c71",
+    "url": "/Currrent-Weather-App/index.html"
+  },
+  {
+    "revision": "0752ec44237dce2e7dc0",
+    "url": "/Currrent-Weather-App/static/css/main.07d70a58.chunk.css"
+  },
+  {
+    "revision": "68ff0c1ddd2946f878e0",
+    "url": "/Currrent-Weather-App/static/js/2.f876c1dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currrent-Weather-App/static/js/2.f876c1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0752ec44237dce2e7dc0",
+    "url": "/Currrent-Weather-App/static/js/main.031a684a.chunk.js"
+  },
+  {
+    "revision": "0bb50f7f77496c55ff2e",
+    "url": "/Currrent-Weather-App/static/js/runtime-main.bc13622a.js"
+  },
+  {
+    "revision": "11e7d75a0ca599bea2d15fb289a00189",
+    "url": "/Currrent-Weather-App/static/media/cloudy.11e7d75a.jpg"
+  },
+  {
+    "revision": "6ddf920e55a8ba1be83804fa87515d4f",
+    "url": "/Currrent-Weather-App/static/media/rainy.6ddf920e.png"
+  },
+  {
+    "revision": "e94748e1810e23b517bf80da5427495c",
+    "url": "/Currrent-Weather-App/static/media/sunny.e94748e1.png"
+  }
+]);
